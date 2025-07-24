ebe51557c6f2a18852d1ea8e1b79b3794acc3cc4
Tai-SE173015 <taibeo161023@gmail.com>
2025-07-24 15:48:58 +0700

    add middleware Authorization

--- src/modules/doctorProfile/doctorProfile.routes.js
+++ src/modules/doctorProfile/doctorProfile.routes.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const router = express.Router();
 const doctorProfileController = require('./doctorProfile.controller');
+const { authMiddleware, restrictTo } = require('../../middlewares/auth.middleware');
 
 router.get('/doctor-profiles', doctorProfileController.getAllDoctorProfiles);
+// router.get('/doctor-profiles', authMiddleware, restrictTo('Doctor'), doctorProfileController.getAllDoctorProfiles);
 router.get('/doctor-profiles/search', doctorProfileController.searchDoctorProfilesByName); // Thêm endpoint tìm kiếm
 router.get('/doctor-profiles/:id', doctorProfileController.getDoctorProfileById);
 router.get('/specialties', doctorProfileController.getAllSpecialties);
+
+
 router.post('/doctor-profiles', doctorProfileController.createDoctorProfile);
 router.put('/doctor-profiles/:id', doctorProfileController.updateDoctorProfile);
 router.delete('/doctor-profiles/:id', doctorProfileController.deleteDoctorProfile);
