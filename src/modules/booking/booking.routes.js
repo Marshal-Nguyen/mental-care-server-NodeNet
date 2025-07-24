2db33f7eef8b167e8f92c1a734497c4c48bbd061
Tai-SE173015 <taibeo161023@gmail.com>
2025-07-24 22:44:14 +0700

    add middleware auth bookings and medical-records api

--- src/modules/booking/booking.routes.js
+++ src/modules/booking/booking.routes.js
@@ -1,26 +1,37 @@
 const express = require("express");
 const bookingController = require("../../modules/booking/booking.controller");
-const requestUser = require("../../middlewares/requireUser");
+const { authMiddleware, restrictTo } = require("../../middlewares/auth.middleware");
+
 const bookingRouter = express.Router();
 
-bookingRouter.get("/bookings", bookingController.getBookings);
+bookingRouter.get("/bookings", authMiddleware, restrictTo('Doctor', 'User', 'Manager'), bookingController.getBookings);
 
 bookingRouter.post(
   "/createBooking",
-  requestUser,
+  authMiddleware,
+  restrictTo('Doctor', 'User', 'Manager'),
   bookingController.createBooking
 );
 
 bookingRouter.post(
   "/updateStatus/:bookingId",
+  authMiddleware,
+  restrictTo('Doctor', 'User', 'Manager'),
   bookingController.updateBookingStatus
 );
 
 bookingRouter.post(
   "/cancelBooking/:bookingId",
+  authMiddleware,
+  restrictTo('Doctor', 'User', 'Manager'),
   bookingController.cancelBooking
 );
 
-bookingRouter.post("/autoCancelBooking", bookingController.autoCancelBookings);
+bookingRouter.post(
+  "/autoCancelBooking",
+  authMiddleware,
+  restrictTo('Doctor', 'User', 'Manager'),
+  bookingController.autoCancelBookings
+);
 
 module.exports = bookingRouter;
--- src/modules/medicalRecord/medicalRecordRouter.js
+++ src/modules/medicalRecord/medicalRecordRouter.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const router = express.Router();
 const medicalRecordController = require('./medicalRecordController');
+const { authMiddleware, restrictTo } = require('../../middlewares/auth.middleware');
 
-router.post('/medical-records', medicalRecordController.createMedicalRecord);
-router.get('/:id', medicalRecordController.getMedicalRecord);
-router.get('/medical-records/:patientId', medicalRecordController.getMedicalRecordsByPatientId);
-router.get('/medical-records/doctor/:doctorId', medicalRecordController.getMedicalRecordsByDoctorId);
-router.put('/:id', medicalRecordController.updateMedicalRecord);
-router.put('/medical-records/:patientId', medicalRecordController.updateMedicalRecordsByPatientId);
-router.delete('/medical-records/:id', medicalRecordController.deleteMedicalRecord);
+router.post('/medical-records', authMiddleware, restrictTo('Doctor', 'User', 'Manager'), medicalRecordController.createMedicalRecord);
+router.get('/:id', authMiddleware, restrictTo('Doctor', 'User', 'Manager'), medicalRecordController.getMedicalRecord);
+router.get('/medical-records/:patientId', authMiddleware, restrictTo('Doctor', 'User', 'Manager'), medicalRecordController.getMedicalRecordsByPatientId);
+router.get('/medical-records/doctor/:doctorId', authMiddleware, restrictTo('Doctor', 'User', 'Manager'), medicalRecordController.getMedicalRecordsByDoctorId);
+router.put('/:id', authMiddleware, restrictTo('Doctor', 'User', 'Manager'), medicalRecordController.updateMedicalRecord);
+router.put('/medical-records/:patientId', authMiddleware, restrictTo('Doctor', 'User', 'Manager'), medicalRecordController.updateMedicalRecordsByPatientId);
+router.delete('/medical-records/:id', authMiddleware, restrictTo('Doctor', 'User', 'Manager'), medicalRecordController.deleteMedicalRecord);
 
 module.exports = router;
