e957b19f5480d50f4a40948844aa0f9f3486a4c5
Tai-SE173015 <taibeo161023@gmail.com>
2025-07-26 12:35:07 +0700

    update

--- src/modules/doctorProfile/doctorProfile.routes.js
+++ src/modules/doctorProfile/doctorProfile.routes.js
@@ -10,8 +10,11 @@ router.get('/doctor-profiles/:id', doctorProfileController.getDoctorProfileById)
 router.get('/specialties', doctorProfileController.getAllSpecialties);
 
 
-router.post('/doctor-profiles', authMiddleware, restrictTo('Doctor', 'Manager'), doctorProfileController.createDoctorProfile);
-router.put('/doctor-profiles/:id', authMiddleware, restrictTo('Doctor', 'Manager'), doctorProfileController.updateDoctorProfile);
-router.delete('/doctor-profiles/:id', authMiddleware, restrictTo('Doctor', 'Manager'), doctorProfileController.deleteDoctorProfile);
+router.post('/doctor-profiles', doctorProfileController.createDoctorProfile);
+router.put('/doctor-profiles/:id', doctorProfileController.updateDoctorProfile);
+router.delete('/doctor-profiles/:id', doctorProfileController.deleteDoctorProfile);
+// router.post('/doctor-profiles', authMiddleware, restrictTo('Doctor', 'Manager'), doctorProfileController.createDoctorProfile);
+// router.put('/doctor-profiles/:id', authMiddleware, restrictTo('Doctor', 'Manager'), doctorProfileController.updateDoctorProfile);
+// router.delete('/doctor-profiles/:id', authMiddleware, restrictTo('Doctor', 'Manager'), doctorProfileController.deleteDoctorProfile);
 
 module.exports = router;
