3c29f51af2d7eb14c35d0d89f3ec6a5936dc6ac9
trizo1104 <vominhtri221104@gmail.com>
2025-07-26 12:22:40 +0700

    update booking and handle refund

--- src/modules/payment/payment.route.js
+++ src/modules/payment/payment.route.js
@@ -1,15 +1,43 @@
 const express = require("express");
 const paymentController = require("../payment/payment.controller");
 const paymentZalo = express.Router();
-const { authMiddleware, restrictTo } = require('../../middlewares/auth.middleware');
+const {
+  authMiddleware,
+  restrictTo,
+} = require("../../middlewares/auth.middleware");
 
-paymentZalo.post("/pay-booking", authMiddleware, restrictTo('User', 'Manager'), paymentController.paymentZalo);
+paymentZalo.post(
+  "/pay-booking",
+  authMiddleware,
+  restrictTo("User", "Manager"),
+  paymentController.paymentZalo
+);
 
-paymentZalo.post(`/callback`, authMiddleware, restrictTo('User', 'Manager'), paymentController.paymentCallback);
+paymentZalo.post(
+  `/callback`,
+  authMiddleware,
+  restrictTo("User", "Manager"),
+  paymentController.paymentCallback
+);
 
-paymentZalo.get("/check-payment-status/:transId", authMiddleware, restrictTo('User', 'Manager'), paymentController.checkPayment);
+paymentZalo.get(
+  "/check-payment-status/:transId",
+  authMiddleware,
+  restrictTo("User", "Manager"),
+  paymentController.checkPayment
+);
 
-paymentZalo.get("/", authMiddleware, restrictTo('User', 'Manager'), paymentController.getPayment);
+paymentZalo.get(
+  "/",
+  authMiddleware,
+  restrictTo("User", "Manager"),
+  paymentController.getPayment
+);
 
-paymentZalo.get("/daily-total", authMiddleware, restrictTo('User', 'Manager'), paymentController.getDailyTotal);
+paymentZalo.get(
+  "/daily-total",
+  authMiddleware,
+  restrictTo("User", "Manager"),
+  paymentController.getDailyTotal
+);
 module.exports = paymentZalo;
