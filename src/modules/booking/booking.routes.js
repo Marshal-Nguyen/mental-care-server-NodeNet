0f657ec45b4a54a86f739f69bfed24a2ef2a092e
trizo1104 <vominhtri221104@gmail.com>
2025-07-31 09:25:50 +0700

    fix auth

--- src/modules/booking/booking.routes.js
+++ src/modules/booking/booking.routes.js
@@ -1,36 +1,44 @@
 const express = require("express");
 const bookingController = require("../../modules/booking/booking.controller");
-const { authMiddleware, restrictTo } = require("../../middlewares/auth.middleware");
+const {
+  authMiddleware,
+  restrictTo,
+} = require("../../middlewares/auth.middleware");
 
 const bookingRouter = express.Router();
 
-bookingRouter.get("/bookings", authMiddleware, restrictTo('Doctor', 'User', 'Manager'), bookingController.getBookings);
+bookingRouter.get(
+  "/bookings",
+  authMiddleware,
+  restrictTo("Doctor", "User", "Manager"),
+  bookingController.getBookings
+);
 
 bookingRouter.post(
   "/createBooking",
   authMiddleware,
-  restrictTo('Doctor', 'User', 'Manager'),
+  restrictTo("Doctor", "User", "Manager"),
   bookingController.createBooking
 );
 
 bookingRouter.post(
   "/updateStatus/:bookingId",
   authMiddleware,
-  restrictTo('Doctor', 'User', 'Manager'),
+  restrictTo("Doctor", "User", "Manager"),
   bookingController.updateBookingStatus
 );
 
 bookingRouter.post(
   "/cancelBooking/:bookingId",
   authMiddleware,
-  restrictTo('Doctor', 'User', 'Manager'),
+  restrictTo("Doctor", "User", "Manager"),
   bookingController.cancelBooking
 );
 
 bookingRouter.post(
   "/autoCancelBooking",
-  authMiddleware,
-  restrictTo('Doctor', 'User', 'Manager'),
+  // authMiddleware,
+  restrictTo("Doctor", "User", "Manager"),
   bookingController.autoCancelBookings
 );
 
